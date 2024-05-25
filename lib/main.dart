fab0f7b5545b749532b3550970e392fc3faa91ad
puzzleweaver <puzzleweaver@gmail.com>
2024-05-25 14:58:36 -0500

    completed basic tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,95 @@ void main() {
   runApp(MyApp());
 }
 
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+
+        // ↓ Make the following change.
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = FavoritesPage();
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600, // ← Here.
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -19,7 +108,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
             fontFamily: "Courier"),
-        home: MyHomePage(),
+        home: HomePage(),
       ),
     );
   }
@@ -27,93 +116,91 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-  var board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
-  var n = 0;
-  void increment() {
-    n++;
+
+  void getNext() {
+    current = WordPair.random();
     notifyListeners();
   }
 
-  int getBoard(int i) {
-    if (i < 0 || i > 15) return -1;
-    return board[i];
-  }
+  var favorites = <WordPair>[];
 
-  void swap(int i, int j) {
-    var tmp = board[i];
-    board[i] = board[j];
-    board[j] = tmp;
-    notifyListeners();
-  }
-
-  void tap(int i) {
-    for (var j in [
-      if (i % 4 != 3) i + 1,
-      if (i % 4 != 0) i - 1,
-      i + 4,
-      i - 4
-    ]) {
-      if (getBoard(j) == 0) {
-        swap(i, j);
-        return;
-      }
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
     }
-  }
-
-  void reset() {
-    for (var i = 0; i < 16; i++) {
-      board[i] = i + 1;
-    }
-    board[15] = 0;
     notifyListeners();
   }
 }
 
-Widget getButt(int i, MyAppState state) {
-  var val = state.board[i];
-  return TextButton(
-      style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all<Color>(
-              val != 0 ? Colors.blue : Colors.white)),
-      onPressed: () => {state.tap(i)},
-      child: Text(
-        "$val",
-        style: new TextStyle(
-          fontSize: 20.0,
-          color: Colors.white,
-        ),
-      ));
-}
-
-class MyHomePage extends StatelessWidget {
+class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min, // shrink it so it centers vertically
-          children: [
-            Text('State: ${appState.current.asLowerCase}'),
-            Text('Fifteen Puzzle:'),
-            for (var i = 0; i < 4; i++)
-              Row(
-                  mainAxisSize:
-                      MainAxisSize.min, // shrink it so it centers vertically,
-                  children: [
-                    for (var j = 0; j < 4; j++) getButt(i * 4 + j, appState),
-                  ]),
-            Text(""),
-            TextButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-                  foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                ),
-                onPressed: appState.reset,
-                child: Text("reset"))
-          ],
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
         ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
